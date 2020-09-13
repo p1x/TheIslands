0c5b09f6419c44aeaaa1d8a4a82f73a47b794204
p1x <p1xel.com@gmail.com>
2020-09-14 00:10:15 +0300

    Add some properties for field rendering.

--- Assets/Scripts/FieldEditor.cs
+++ Assets/Scripts/FieldEditor.cs
@@ -9,7 +9,14 @@ namespace TheIslands {
         public const float MinSamplingRate = 0.1f;
         
         private MeshEditorSettingsWindow _settingsWindow;
-        private float _scale = 0.1f;
+        private float _minSize = 0.1f;
+        private float _maxSize = 1f;
+
+        private float _minValue = 0.4f;
+        private float _maxValue = 1f;
+        
+        private Color _color = Color.blue;
+        
         private float _fieldSamplingRate = 1f;
         
         private void OnEnable() => EditorTools.activeToolChanged += ActiveToolDidChange;
@@ -50,47 +57,76 @@ namespace TheIslands {
             var evt = Event.current;
             if (evt.type == EventType.Repaint) {
                 if (target is MeshGenerator meshGenerator && meshGenerator.Field is IScalarField field) {
-                    if (_fieldSamplingRate < MinSamplingRate)
-                        return;
-                    
-                    var step = 1 / _fieldSamplingRate;
-
-                    var transform = Camera.current.transform;
-                    var right     = transform.right;
-                    var up        = transform.up;
-                    var lt        = up - right;
-                    var rt        = up + right;
-                    var rb        = -up + right;
-                    var lb        = -up - right;
-
-                    Material.SetPass(0);
-                    GL.PushMatrix();
-                    GL.Begin(GL.QUADS);
-                    
-                    for (float x = 0; x < meshGenerator.size.x; x += step)
-                    for (float y = 0; y < meshGenerator.size.y; y += step)
-                    for (float z = 0; z < meshGenerator.size.z; z += step) {
-                        var position = new Vector3(x, y, z);
-                        var value    = Mathf.Clamp01(field.GetValue(position));
-                        var scale    = value * _scale;
-
-                        GL.Color(Color.blue);
-                        GL.Vertex(position + lt * scale);
-                        GL.Vertex(position + rt * scale);
-                        GL.Vertex(position + rb * scale);
-                        GL.Vertex(position + lb * scale);
-                    }
-                    GL.End();
-                    GL.PopMatrix();
+                    RenderField(field, meshGenerator.size);
                 }
             }
         }
 
+        private void RenderField(IScalarField field, Vector3 size) {
+            if (_fieldSamplingRate < MinSamplingRate)
+                return;
+
+            var step = 1 / _fieldSamplingRate;
+
+            var transform = Camera.current.transform;
+            var right     = transform.right;
+            var up        = transform.up;
+            var lt        = up - right;
+            var rt        = up + right;
+            var rb        = -up + right;
+            var lb        = -up - right;
+
+            Material.SetPass(0);
+            GL.PushMatrix();
+            GL.Begin(GL.QUADS);
+            GL.Color(_color);
+
+            for (float x = 0; x < size.x; x += step)
+            for (float y = 0; y < size.y; y += step)
+            for (float z = 0; z < size.z; z += step) {
+                var position    = new Vector3(x, y, z);
+                var rawValue    = field.GetValue(position);
+                var scaledValue = Mathf.InverseLerp(_minValue, _maxValue, rawValue);
+                if (scaledValue > 0 && scaledValue < 1) {
+                    var value = Mathf.Clamp01(scaledValue);
+                    var scale = Mathf.Lerp(_minSize, _maxSize, value);
+
+                    GL.Vertex(position + lt * scale);
+                    GL.Vertex(position + rt * scale);
+                    GL.Vertex(position + rb * scale);
+                    GL.Vertex(position + lb * scale);
+                }
+            }
+
+            GL.End();
+            GL.PopMatrix();
+        }
+
         private void OnPropertyChanged() => SceneView.RepaintAll();
         
-        public float Scale {
-            get => _scale;
-            set => Utils.RiseAndSetIfChanged(ref _scale, value, OnPropertyChanged);
+        public float MinSize {
+            get => _minSize;
+            set => Utils.RiseAndSetIfChanged(ref _minSize, value, OnPropertyChanged);
+        }
+        
+        public float MaxSize {
+            get => _maxSize;
+            set => Utils.RiseAndSetIfChanged(ref _maxSize, value, OnPropertyChanged);
+        }
+        
+        public float MinValue {
+            get => _minValue;
+            set => Utils.RiseAndSetIfChanged(ref _minValue, value, OnPropertyChanged);
+        }
+
+        public float MaxValue {
+            get => _maxValue;
+            set => Utils.RiseAndSetIfChanged(ref _maxValue, value, OnPropertyChanged);
+        }
+
+        public Color Color {
+            get => _color;
+            set => Utils.RiseAndSetIfChanged(ref _color, value, OnPropertyChanged);
         }
 
         public float FieldSamplingRate {
--- Assets/Scripts/MeshEditorSettingsWindow.cs
+++ Assets/Scripts/MeshEditorSettingsWindow.cs
@@ -14,8 +14,12 @@ namespace TheIslands {
         }
 
         private void OnGUI() {
-            _editor.Scale             = EditorGUILayout.Slider("Point Size:", _editor.Scale, 0, 1);
             _editor.FieldSamplingRate = EditorGUILayout.FloatField("Sampling Rate:", _editor.FieldSamplingRate);
+            _editor.MinSize           = EditorGUILayout.Slider("Point Min Size:", _editor.MinSize,  0,                _editor.MaxSize);
+            _editor.MaxSize           = EditorGUILayout.Slider("Point Max Size:", _editor.MaxSize,  _editor.MinSize,  1);
+            _editor.MinValue          = EditorGUILayout.Slider("Min Value:",      _editor.MinValue, 0,                _editor.MaxValue);
+            _editor.MaxValue          = EditorGUILayout.Slider("Max Value:",      _editor.MaxValue, _editor.MinValue, 1);
+            _editor.Color             = EditorGUILayout.ColorField("Point Color:", _editor.Color);
         }
     }
 }

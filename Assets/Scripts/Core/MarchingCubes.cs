63c5bd6ad29724a9b254831640c4330d724cda7d
p1x <p1xel.com@gmail.com>
2020-09-28 04:12:11 +0300

    Add empty marching-cube implementation.
    Signed-off-by: p1x <p1xel.com@gmail.com>

---
+++ Assets/Scripts/Core/MarchingCubes.cs
@@ -0,0 +1,7 @@
+﻿namespace TheIslands.Core {
+    public class MarchingCubes {
+        public void Polygonize(IScalarField field, float isoValue, Size3 step, Size3Int size, IMeshBuilder builder) {
+            
+        }
+    }
+}

2e7d1144120765c9c266819d198e00d50330164b
p1x <p1xel.com@gmail.com>
2020-11-14 04:17:33 +0300

    Add nice start and end Caps to DirectionalField in SceneView.
    Signed-off-by: p1x <p1xel.com@gmail.com>

--- Assets/Scripts/Editor/FieldEditorTool/DirectionalFieldTool.cs
+++ Assets/Scripts/Editor/FieldEditorTool/DirectionalFieldTool.cs
@@ -7,10 +7,12 @@ namespace TheIslands.Editor.FieldEditorTool {
     [Preserve]
     public sealed class DirectionalFieldTool : ScalarFieldTool<DirectionalField> {
         protected override void OnToolGUI(DirectionalField field) {
+            Handles.DrawLine(field.Start, field.End);
+            Handles.CubeHandleCap(0, field.Start, Quaternion.identity, HandleUtility.GetHandleSize(field.Start) * 0.1f, EventType.Repaint);
+            Handles.ConeHandleCap(0, field.End, Quaternion.LookRotation(field.End - field.Start), HandleUtility.GetHandleSize(field.End) * 0.2f, EventType.Repaint);
+            
             field.Start = Handles.PositionHandle(field.Start, Quaternion.identity);
             field.End = Handles.PositionHandle(field.End, Quaternion.identity);
-            
-            Handles.DrawLine(field.Start, field.End);
         }
     }
 }
